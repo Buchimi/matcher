1082e2aa6255e3e6fa577d9c67865a148a1fa416
Buchimi <stormzzy900@gmail.com>
2024-02-08 01:43:54 -0600

    update package name

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='Hungarian_Matcher',
+    name='Valentine_Matcher_2024',
     version='0.1',
     packages=find_packages(),
     # Add other metadata as needed
